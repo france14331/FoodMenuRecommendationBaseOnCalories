3f656bed87fd1631bc4cab73d7e329c730005cde
FranceGD <59070042@it.kmitl.ac.th>
2019-10-14 21:15:28 +0700

    Create signin page

---
+++ frontend/src/components/Signin.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container-fluid justify-content-center">
+        <div class="row">
+            <div id="signin-form" class="col-4 offset-4 text-center">
+                <div>
+                    <p><label><b>Username : </b></label>
+                    <input type="text" placeholder="Username"></p>
+
+                    <p><label><b>Password : </b></label>
+                    <input type="password" placeholder="Password"></p>
+                </div>
+
+                <div>
+                    <button type="button" class="btn btn-link">สมัครสมาชิก</button>
+                    <button type="button" class="btn btn-primary">เข้าสู่ระบบ</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signin'
+}
+</script>
+
+<style>
+    #signin-form {
+        background-color: lightgray; 
+        padding: 5%;
+        margin-top: 100px;
+        width: 500px;
+        border-radius: 3em;
+    }
+    input{
+        border: 0px solid;
+        border-radius: 0.3em
+    }
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,20 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Index from '@/components/Index'
+import Signin from '@/components/Signin'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
       path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/index',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
-  ]
+  ],
+  mode: 'history'
 })
